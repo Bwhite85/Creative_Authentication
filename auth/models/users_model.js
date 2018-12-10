743e99140a74c01532ba88b52592c469f069e6a5
Chester Davison <chesterd02@gmail.com>
2018-12-10 22:22:47 +0000

    added schema

--- auth/models/users_model.js
+++ auth/models/users_model.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     username: { type: String, unique: true },
     email: String,
-    color: String,
+    SS: String,
+    firstPet: String,
+    momMaiden: String,
     hashed_password: String
 });
 mongoose.model('User', UserSchema);
